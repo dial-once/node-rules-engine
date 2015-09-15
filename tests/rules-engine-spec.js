f914f21ace53af358d45da2b4422fdb82c186b99
Jonathan Muller <koren@pukogames.com>
2015-09-15 18:17:08 +0200

    Add more check on parameters

--- tests/rules-engine-spec.js
+++ tests/rules-engine-spec.js
@@ -9,12 +9,49 @@ describe('rule engine entry point', function() {
     expect(typeof rEngine.apply).toBe('function');
   });
 
-  it('should trigger an exception when called with bad params', function(done) {
+  it('should trigger an exception when called with no params', function(done) {
     rEngine.apply()
     .then(function(){
       done(new Error('It should have failed'));
     })
     .catch(function(){
+      done();
+    });
+  });
+
+  it('should trigger an exception when called with bad params', function(done) {
+    rEngine.apply({}, {})
+    .then(function(){
+      done(new Error('It should have failed'));
+    })
+    .catch(function(){
+      done();
+    });
+  });
+
+  it('should trigger an exception when called with invalid events but valid specs format', function(done) {
+    rEngine.apply({}, [])
+    .then(function(){
+      done(new Error('It should have failed'));
+    })
+    .catch(function(){
+      done();
+    });
+  });
+
+  it('should trigger an exception when called with valid events but invalid specs format', function(done) {
+    rEngine.apply([], {})
+    .then(function(){
+      done(new Error('It should have failed'));
+    })
+    .catch(function(){
+      done();
+    });
+  });
+
+  it('should get true when params are valid but empty', function(done) {
+    rEngine.apply([], [])
+    .then(function(){
       done();
     });
   });
