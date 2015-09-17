bed51f8c106eb34a332472c314dfece6a3137e24
Jonathan Muller <koren@pukogames.com>
2015-09-17 12:25:34 +0200

    Add more complex tests by using rather large array of events/specs

--- tests/rules-engine-spec.js
+++ tests/rules-engine-spec.js
@@ -46,6 +46,9 @@ describe('rule engine entry point', function() {
       })
       .catch(doneCallback(done));
   });
+});
+
+describe('rule engine usage', function() {
 
   it('should get false when event list is empty (default is no-match)', function(done) {
     rEngine.apply([], [])
@@ -149,5 +152,94 @@ describe('rule engine entry point', function() {
         expect(res).toBe(true);
         done();
       });
+  });
+});
+
+describe('rule engine complex usage', function(){
+  var usage = [
+    {'view': {val: 1}},
+    {'view': {val: 2}},
+    {'view': {val: 3}},
+    {'click': {val: true}},
+    {'random': {val: 30}}
+  ];
+
+  it('should match with many event matching many rules (even count)', function(done){
+    rEngine.apply(usage, [
+      {'view': {val: 1, should: true}},
+      {'view': {val: 2, should: true}},
+      {'view': {val: 3, should: true}},
+      {'click': {val: true, should: true}},
+      {'random': {val: 30, should: true}}
+    ])
+    .then(function(res){
+      expect(res).toBe(true);
+      done();
+    });
+  });
+
+  it('should match with many event matching many rules (uneven count)', function(done){
+    rEngine.apply(usage, [
+      {'view': {val: 1, should: true}},
+      {'view': {val: 2, should: true}},
+      {'view': {val: 3, should: true}},
+      {'click': {val: true, should: true}}
+    ])
+    .then(function(res){
+      expect(res).toBe(true);
+      done();
+    });
+  });
+
+  it('should not match when one of the many event don\'t match many rules', function(done){
+    rEngine.apply(usage, [
+      {'view': {val: 1, should: true}},
+      {'view': {val: 2, should: true}},
+      {'view': {val: 4, should: true}},
+      {'click': {val: true, should: true}},
+      {'random': {val: 30, should: true}}
+    ])
+    .then(function(res){
+      expect(res).not.toBe(true);
+      done();
+    });
+  });
+
+  it('should not match when one of the many event don\'t match one of the rules', function(done){
+    rEngine.apply(usage, [
+      {'view': {val: 1, should: true}},
+      {'view': {val: 2, should: true}},
+      {'view': {val: 3, should: false}},
+      {'click': {val: true, should: true}},
+      {'random': {val: 30, should: true}}
+    ])
+    .then(function(res){
+      expect(res).not.toBe(true);
+      done();
+    });
+  });
+
+  it('should match when events match specifications (using array of values)', function(done){
+    rEngine.apply(usage, [
+      {'view': {val: [1,2,3], should: true}},
+      {'click': {val: true, should: true}},
+      {'random': {val: 30, should: true}}
+    ])
+    .then(function(res){
+      expect(res).toBe(true);
+      done();
+    });
+  });
+
+  it('should match when events match specifications (using wildcard)', function(done){
+    rEngine.apply(usage, [
+      {'view': {val: '*', should: true}},
+      {'click': {val: true, should: true}},
+      {'random': {val: 30, should: true}}
+    ])
+    .then(function(res){
+      expect(res).toBe(true);
+      done();
+    });
   });
 });
